6fa18ed1ba4653a457e18e9c2910d6b13db96c6a
Phillip England <phillipengland@jacex.local>
2023-11-19 12:36:56 -0600

    patched logout error on team account

--- pkg/routes/pageRoutes/LogoutPage.go
+++ pkg/routes/pageRoutes/LogoutPage.go
@@ -3,6 +3,6 @@ package pageRoutes
 import "github.com/gin-gonic/gin"
 
 func LogoutPage(c *gin.Context) {
-	c.SetCookie("session", "", -1, "/", "", false, true) // Delete the session cookie
+	c.SetCookie("session", "", -1, "/", "", true, true) // Delete the session cookie
 	c.Redirect(301, "/") // Redirect to the root path
 }
