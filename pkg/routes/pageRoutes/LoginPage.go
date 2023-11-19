562a9a3899714814eb418e6b89f684a35b2d7502
Phillip England <phillipengland@jacex.local>
2023-11-19 13:06:40 -0600

    patched logout cache errors

--- pkg/routes/pageRoutes/LoginPage.go
+++ pkg/routes/pageRoutes/LoginPage.go
@@ -1,21 +1,12 @@
 package pageRoutes
 
 import (
-	"fluffy-duck/pkg/middleware"
 	"fluffy-duck/pkg/render"
 
 	"github.com/gin-gonic/gin"
 )
 
 func LoginPage(c *gin.Context) {
-	userType, _ := middleware.Auth(c)
-	if userType == "team" {
-		c.Redirect(301, "/team")
-		return
-	}
-	if userType == "admin" {
-		c.Redirect(301, "/admin")
-		return
-	}
+	c.SetCookie("session", "", -1, "/", "", true, true) // Delete the session cookie
 	c.Data(200, "text/html", []byte(render.LoginPage(c)))
 }
